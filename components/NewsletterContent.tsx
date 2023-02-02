610a7c4441276a87a3feb997b89dde877a3ded31
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-02-02 23:02:33 +0100

    newsletter ready

--- components/NewsletterContent.tsx
+++ components/NewsletterContent.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
 export const NewsletterContent = () => {
-  const [email, setEmail] = useState(null);
   const [submitted, setSubmitted] = useState(false);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    if (email === null) return;
-
-    await fetch("/api/subscribeUser", {
+  const onSubmit = (data) => {
+    console.log(data);
+    fetch("/api/subscribeUser", {
       body: JSON.stringify({
-        email: email,
+        email: data.email,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -25,8 +30,9 @@ export const NewsletterContent = () => {
   if (submitted) {
     return (
       <div className="content bg-black flex flex-col px-6 text-center md:text-left md:flex-row justify-between items-center rounded-lg shadow-lg py-6 md:w-2/3 md:px-10">
-        <h1 className="text-yellow-500 md:text-3xl py-7 w-full text-center font-bold">
-          Thanks for your email! :){" "}
+        <h1 className="text-yellow-500 md:text-2xl py-7 w-full text-center font-bold">
+          Thank you for being a valued customer. Hundreds of clients have
+          trusted us, and we appreciate your continued support.{" "}
         </h1>
       </div>
     );
@@ -39,26 +45,44 @@ export const NewsletterContent = () => {
           <h2 className="md:text-2xl text-xl text-white mb-2 md:mb-0">
             subscribe to our newsletter
           </h2>
-          <p className="text-neutral-500 text-xs md:text-sm w-3/4">
-            Lorem ipsum dolor sit amet consectetur
+          <p className="text-neutral-500 text-xs md:text-sm w-3/4 md:w-3/2">
+            Get access to the world of exclusive steering wheels and stay
+            updated on the latest news and best offers.
           </p>
         </div>
         <form
-          onSubmit={submitHandler}
-          className="flex justify-center mt-6 md:mt-0 h-10 md:h-12"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row items-center md:items-stretch gap-3 justify-center mt-6 md:mt-0 h-10 md:h-12"
         >
-          <input
-            type="text"
-            placeholder="your eamil"
-            onChange={(e) => setEmail(e.target.value)}
-            className="rounded-l-lg text-center w-1/2 md:w-full"
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => (
+              <div className=" flex items-center">
+                <p className="text-red-500 font-bold">{message}</p>
+              </div>
+            )}
           />
-          <button
-            type="submit"
-            className="bg-yellow-500 px-4 text-white font-bold rounded-r-lg"
-          >
-            Send
-          </button>
+          <div className="flex justify-center">
+            <input
+              {...register("email", {
+                required: "email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "email must contain @",
+                },
+              })}
+              placeholder="your eamil"
+              className="rounded-l-lg text-center h-10 md:h-auto w-1/2 md:w-full"
+            />
+
+            <button
+              type="submit"
+              className="bg-yellow-500 px-4 text-white font-bold rounded-r-lg"
+            >
+              Send
+            </button>
+          </div>
         </form>
       </div>
     </>
--- pages/api/subscribeUser.ts
+++ pages/api/subscribeUser.ts
@@ -1,33 +1,41 @@
+import axios from "axios";
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   const { email } = req.body;
 
-  if (!email) {
+  if (!email || !email.length) {
     return res.status(400).json({ error: "Email is required" });
   }
 
+  const API_KEY = process.env.MAILCHIMP_API_KEY;
+  const API_SERVER = process.env.MAILCHIMP_API_SERVER;
+  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
+
+  const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`;
+
+  const data = {
+    email_address: email,
+    status: "subscribed",
+  };
+
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `api_key ${API_KEY}`,
+    },
+  };
+
   try {
-    const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
-    const API_KEY = process.env.MAILCHIMP_API_KEY;
-    const DATACENTER = process.env.MAILCHIMP_API_SERVER;
-
-    const data = {
-      email_address: email,
-      status: "subscribed",
-    };
-
-    await fetch(
-      `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
-      {
-        body: JSON.stringify(data),
-        headers: {
-          Authorization: `apikey ${API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      }
-    );
+    const response = await axios.post(url, data, options);
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: `There was an error subscribing to the newsletter. Shoot me an email at info@dafoxdesign.com and we'll add you to the list.`,
+      });
+    }
+    return res.status(201).json({ message: "success" });
   } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
+    console.log(error);
+    return res.status(500).json({ error: error.message });
   }
 };

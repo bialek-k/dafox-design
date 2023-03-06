567453f28aeefca9fadf7c5bfdd8748336999021
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-03-06 12:27:26 +0100

    update search

--- pages/shop/search/index.tsx
+++ pages/shop/search/index.tsx
@@ -5,13 +5,14 @@ import { client } from "../../../lib/apollo";
 import { gql } from "@apollo/client";
 import Hero from "../../../components/Hero";
 
-const SearchPage = ({ data, totalProductNumber }) => {
+const SearchPage = ({ data, totalProductNumber, bestsellerProducts }) => {
   return (
     <div className="flex flex-col justify-center items-center">
       {/* <Hero /> */}
       <ProductListContainer
         products={data}
         totalProducts={totalProductNumber}
+        bestsellerProducts={bestsellerProducts}
       />
     </div>
   );
@@ -68,6 +69,41 @@ export async function getServerSideProps(context) {
     }
   `;
 
+  const bestsellerQuery = gql`
+    query MyQuery {
+      bestsellerProducts: allProducts(filter: { bestseller: { eq: "true" } }) {
+        name
+        id
+        slug
+        bestseller
+        price
+        freeShipping
+        promotion
+        inStock
+        category {
+          id
+          series
+          name
+        }
+        image {
+          responsiveImage {
+            alt
+            base64
+            bgColor
+            title
+            aspectRatio
+            height
+            sizes
+            src
+            srcSet
+            webpSrcSet
+            width
+          }
+        }
+      }
+    }
+  `;
+
   const skip = 15 * (context.query.page - 1);
 
   const { data } = await client.query({
@@ -79,9 +115,15 @@ export async function getServerSideProps(context) {
     },
   });
 
+  const bestData = await client.query({
+    query: bestsellerQuery,
+  });
+
   return {
     props: {
       data: data.paginatedSearchProducts,
+      bestsellerProducts: bestData.data.bestsellerProducts,
+
       totalProductNumber: data._allProductsMeta.count,
     },
   };

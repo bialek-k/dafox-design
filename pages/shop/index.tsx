17d3732acf37259bd2702c21867908f422703f4e
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-03-06 12:24:33 +0100

    again update index

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -3,13 +3,18 @@ import { client } from "../../lib/apollo";
 import { gql } from "@apollo/client";
 import Hero from "../../components/Hero";
 
-const Page = ({ products, totalProductNumber }): React.ReactElement => {
+const Page = ({
+  products,
+  totalProductNumber,
+  bestsellerProducts,
+}): React.ReactElement => {
   return (
     <div className="flex flex-col justify-center items-center">
       <Hero />
       <ProductListContainer
         products={products}
         totalProducts={totalProductNumber}
+        bestsellerProducts={bestsellerProducts}
       />
     </div>
   );
@@ -53,14 +58,54 @@ export async function getStaticProps() {
     }
   `;
 
+  const bestsellerQuery = gql`
+    query MyQuery {
+      bestsellerProducts: allProducts(filter: { bestseller: { eq: "true" } }) {
+        name
+        id
+        slug
+        bestseller
+        price
+        freeShipping
+        promotion
+        inStock
+        category {
+          id
+          series
+          name
+        }
+        image {
+          responsiveImage {
+            alt
+            base64
+            bgColor
+            title
+            aspectRatio
+            height
+            sizes
+            src
+            srcSet
+            webpSrcSet
+            width
+          }
+        }
+      }
+    }
+  `;
+
   const { data } = await client.query({
     query: paginatedProductsLists,
     variables: { first: 15, skip: 0 },
   });
 
+  const bestData = await client.query({
+    query: bestsellerQuery,
+  });
+
   return {
     props: {
       products: data.allProducts,
+      bestsellerProducts: bestData.data.bestsellerProducts,
       totalProductNumber: data._allProductsMeta.count,
     },
   };

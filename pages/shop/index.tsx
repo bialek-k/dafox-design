ccebf79fc0491424209ca037033e86dd2df38580
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-03-11 10:18:44 +0100

    update index.tsx

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,36 +1,26 @@
 import { ProductListContainer } from "../../components/ProductList/ProductListContainer";
 import { client } from "../../lib/apollo";
 import { Hero } from "../../components/HeroSection/Hero";
-import {
-  getBestsellerProducts,
-  paginatedProductLists,
-} from "../../lib/DatocmsApiCall";
+import { paginatedProductLists } from "../../lib/DatocmsApiCall";
 
-const Page = ({
-  products,
-  totalProductNumber,
-  bestsellerProducts,
-}): React.ReactElement => {
+const Page = ({ products, totalProductNumber }): React.ReactElement => {
   return (
     <div className="flex flex-col justify-center items-center">
       <Hero />
       <ProductListContainer
         products={products}
         totalProducts={totalProductNumber}
-        bestsellerProducts={bestsellerProducts}
       />
     </div>
   );
 };
 
 export async function getStaticProps() {
-  const bestsellerProducts = await getBestsellerProducts();
   const paginatedProducts = await client.query(paginatedProductLists(0));
 
   return {
     props: {
       products: paginatedProducts.data.allProducts,
-      bestsellerProducts,
       totalProductNumber: paginatedProducts.data._allProductsMeta.count,
     },
   };

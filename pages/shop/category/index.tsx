c359fa9b450aa301cce662378c354b9bab42bf80
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-03-13 00:00:54 +0100

    limited offer category

--- pages/shop/category/index.tsx
+++ pages/shop/category/index.tsx
@@ -6,13 +6,19 @@ import { ElfsightWidget } from "react-elfsight-widget";
 
 import {
   getBestsellerProducts,
+  getLimitedOfferProducts,
   productsByCategoryQuery,
 } from "../../../lib/DatocmsApiCall";
 import { PageTitle } from "../../../components/PageTitle";
 import { ListSettings } from "../../../components/ListSettings/ListSettings";
-import { BestsellerContainer } from "../../../components/ProductList/Bestseller/BestsellerContainer";
+import { SpecialOffersContainer } from "../../../components/ProductList/SpecialOffers/SpecialOffersContainer";
 
-const CategoryPage = ({ data, totalProductNumber, bestsellerProducts }) => {
+const CategoryPage = ({
+  data,
+  totalProductNumber,
+  bestsellerProducts,
+  limitedOfferProducts,
+}) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <PageTitle />
@@ -23,7 +29,21 @@ const CategoryPage = ({ data, totalProductNumber, bestsellerProducts }) => {
       />
       {bestsellerProducts.length > 0 && (
         <GrayBackgroundWrapper>
-          <BestsellerContainer bestsellerProducts={bestsellerProducts} />
+          <SpecialOffersContainer
+            products={bestsellerProducts}
+            title="Bestseller"
+            subtitle="Steering wheels from this collection are the most purchased items by
+            customers"
+          />
+        </GrayBackgroundWrapper>
+      )}
+      {limitedOfferProducts.length > 0 && (
+        <GrayBackgroundWrapper>
+          <SpecialOffersContainer
+            products={limitedOfferProducts}
+            title="Limited Offer Products"
+            subtitle="Get it in the best price!"
+          />
         </GrayBackgroundWrapper>
       )}
 
@@ -42,12 +62,15 @@ export async function getServerSideProps(context) {
   );
 
   const bestsellerProducts = await getBestsellerProducts();
+  const limitedOfferProducts = await getLimitedOfferProducts();
+
   const count = productsByCategory.data.Products_count.length;
 
   return {
     props: {
       data: productsByCategory.data.Paginated,
       bestsellerProducts,
+      limitedOfferProducts,
       totalProductNumber: count,
     },
   };
